606216a12acc6d33edd9d43f945db1d0563b2b7c
Josewowgame2888 <mistertec7777777@gmail.com>
2020-10-30 22:42:09 -0600

    new JsonConfig()

--- PocketNET/Core/Config/JsonConfig.cs
+++ PocketNET/Core/Config/JsonConfig.cs
@@ -2,41 +2,25 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using YamlDotNet.Core;
-using YamlDotNet.RepresentationModel;
-using YamlDotNet.Serialization;
+using System.Text.Json;
 
 namespace PocketNET.Core.Config
 {
-    public class YamlConfig
+    public class JsonConfig
     {
-        private Dictionary<string, object> _config = new Dictionary<string, object>();
+        private Dictionary<string, object> _json = new Dictionary<string, object>();
         private string route { get; set; }
 
-        public YamlConfig(string route)
+        public JsonConfig(string route)
         {
             this.route = route;
-              
+
             try
             {
                 if (File.Exists(route))
                 {
-                    var yaml = new YamlStream();
-
-                    yaml.Load(new StringReader(File.ReadAllText(route)));
-
-                    var mapping = (YamlMappingNode)yaml.Documents[0].RootNode;
-
-                    foreach (var entry in mapping.Children)
-                    {
-                        _config.Add(entry.Key.ToString(), entry.Value);
-                    }
+                    _json = JsonSerializer.Deserialize<Dictionary<string, object>>(File.ReadAllText(route));
                 }
-
-            }
-            catch (YamlException e)
-            {
-                Logger.Error(e.Message);
             }
             catch (IOException e)
             {
@@ -46,245 +30,230 @@ namespace PocketNET.Core.Config
 
         public string GetString(string key)
         {
-            if (!_config.ContainsKey(key))
+            if (!_json.ContainsKey(key))
             {
                 Logger.Warning("Config: Object [key:" + key + "] not found in " + route);
 
                 return null;
             }
 
-            return _config[key].ToString();
+            return _json[key].ToString();
         }
 
         public int GetInt32(string key)
         {
-            if (!_config.ContainsKey(key))
+            if (!_json.ContainsKey(key))
             {
                 Logger.Warning("Config: Object [key:" + key + "] not found in " + route);
 
                 return -1;
             }
 
-            return Convert.ToInt32(_config[key]);
+            return Convert.ToInt32(_json[key]);
         }
 
         public long GetInt64(string key)
         {
-            if (!_config.ContainsKey(key))
+            if (!_json.ContainsKey(key))
             {
                 Logger.Warning("Config: Object [key:" + key + "] not found in " + route);
 
                 return -1;
             }
 
-            return Convert.ToInt64(_config[key]);
+            return Convert.ToInt64(_json[key]);
         }
 
-        public short getInt16(string key)
+        public short GetInt16(string key)
         {
-            if (!_config.ContainsKey(key))
+            if (!_json.ContainsKey(key))
             {
                 Logger.Warning("Config: Object [key:" + key + "] not found in " + route);
 
                 return -1;
             }
 
-            return Convert.ToInt16(_config[key]);
+            return Convert.ToInt16(_json[key]);
         }
 
         public float GetFloat(string key)
         {
-            if (!_config.ContainsKey(key))
+            if (!_json.ContainsKey(key))
             {
                 Logger.Warning("Config: Object [key:" + key + "] not found in " + route);
 
                 return -1;
             }
 
-            return Convert.ToSingle(_config[key]);
+            return Convert.ToSingle(_json[key]);
         }
 
         public double GetDouble(string key)
         {
-            if (!_config.ContainsKey(key))
+            if (!_json.ContainsKey(key))
             {
                 Logger.Warning("Config: Object [key:" + key + "] not found in " + route);
 
                 return -1;
             }
 
-            return Convert.ToDouble(_config[key]);
+            return Convert.ToDouble(_json[key]);
         }
 
         public bool GetBool(string key)
         {
-            if (!_config.ContainsKey(key))
+            if (!_json.ContainsKey(key))
             {
                 Logger.Warning("Config: Object [key:" + key + "] not found in " + route);
 
                 return false;
             }
 
-            return Convert.ToBoolean(_config[key]);
+            return Convert.ToBoolean(_json[key]);
         }
 
         public byte GetByte(string key)
         {
-            if (!_config.ContainsKey(key))
+            if (!_json.ContainsKey(key))
             {
                 Logger.Warning("Config: Object [key:" + key + "] not found in " + route);
 
                 return 0;
             }
 
-            return Convert.ToByte(_config[key]);
+            return Convert.ToByte(_json[key]);
         }
 
         public decimal GetDecimal(string key)
         {
-            if (!_config.ContainsKey(key))
+            if (!_json.ContainsKey(key))
             {
                 Logger.Warning("Config: Object [key:" + key + "] not found in " + route);
 
                 return -1;
             }
 
-            return Convert.ToDecimal(_config[key]);
+            return Convert.ToDecimal(_json[key]);
         }
 
         public sbyte GetSByte(string key)
         {
-            if (!_config.ContainsKey(key))
+            if (!_json.ContainsKey(key))
             {
                 Logger.Warning("Config: Object [key:" + key + "] not found in " + route);
 
                 return -1;
             }
 
-            return Convert.ToSByte(_config[key]);
+            return Convert.ToSByte(_json[key]);
         }
 
         public uint GetUInt32(string key)
         {
-            if (!_config.ContainsKey(key))
+            if (!_json.ContainsKey(key))
             {
                 Logger.Warning("Config: Object [key:" + key + "] not found in " + route);
 
                 return 0;
             }
 
-            return Convert.ToUInt32(_config[key]);
+            return Convert.ToUInt32(_json[key]);
         }
 
         public ulong GetUInt64(string key)
         {
-            if (!_config.ContainsKey(key))
+            if (!_json.ContainsKey(key))
             {
                 Logger.Warning("Config: Object [key:" + key + "] not found in " + route);
 
                 return 0;
             }
 
-            return Convert.ToUInt64(_config[key]);
+            return Convert.ToUInt64(_json[key]);
         }
 
         public ushort GetUShort(string key)
         {
-            if (!_config.ContainsKey(key))
+            if (!_json.ContainsKey(key))
             {
                 Logger.Warning("Config: Object [key:" + key + "] not found in " + route);
 
                 return 0;
             }
 
-            return Convert.ToUInt16(_config[key]);
+            return Convert.ToUInt16(_json[key]);
         }
 
         public object GetObject(string key)
         {
-            if (!_config.ContainsKey(key))
+            if (!_json.ContainsKey(key))
             {
                 Logger.Warning("Config: Object [key:" + key + "] not found in " + route);
 
                 return null;
             }
 
-            return _config[key];
-        }
-
-        public List<object> GetList(string key)
-        {
-            if (!_config.ContainsKey(key))
-            {
-                Logger.Warning("Config: Object [key:" + key + "] not found in " + route);
-
-                return null;
-            }
-
-            return (List<object>)_config[key];
+            return _json[key];
         }
 
         public object Get(string key)
         {
-            if (!_config.ContainsKey(key))
+            if (!_json.ContainsKey(key))
             {
                 Logger.Warning("Config: Object [key:" + key + "] not found in " + route);
 
                 return null;
             }
 
-            return _config[key];
+            return _json[key];
         }
 
         public Dictionary<string, object> GetAll()
         {
-            return _config;
+            return _json;
         }
 
         public void Set(string key, object value)
         {
-            if (!_config.ContainsKey(key))
+            if (!_json.ContainsKey(key))
             {
-                _config.Add(key, value);
+                _json.Add(key, value);
 
                 return;
             }
 
-            _config[key] = value;
+            _json[key] = value;
         }
 
         public bool Remove(string key)
         {
-            if (_config.ContainsKey(key)) return false;
+            if (_json.ContainsKey(key)) return false;
 
-            _config.Remove(key);
+            _json.Remove(key);
 
             return true;
         }
 
         public void Clear()
         {
-            _config.Clear();
+            _json.Clear();
         }
 
         public int Count()
         {
-            return _config.Count;
+            return _json.Count;
         }
 
         public bool Contains(string key)
         {
-            return _config.ContainsKey(key);
-        } 
+            return _json.ContainsKey(key);
+        }
 
         public void Save()
         {
             try
             {
-                var serializer = new SerializerBuilder().Build();
-                var yaml = serializer.Serialize(_config);
-
-                File.WriteAllText(route, yaml);
+                File.WriteAllText(route, JsonSerializer.Serialize(_json));
             }
             catch (IOException e)
             {
--- PocketNET/PocketNET.cs
+++ PocketNET/PocketNET.cs
@@ -8,7 +8,16 @@ namespace PocketNET
     {
         static void Main(string[] args)
         {
- 
+            JsonConfig config = new JsonConfig(@"C:\Users\Jose Luis\Downloads\Servidor/myjson.json");
+
+            config.Set("test", 1);
+            config.Set("test1", "jose");
+            config.Set("test2", true);
+            config.Save();
+
+            Console.WriteLine(config.GetInt32("test"));
+            Console.WriteLine(config.GetString("test1"));
+            Console.WriteLine(config.GetBool("test2"));
         }
     }
 }
