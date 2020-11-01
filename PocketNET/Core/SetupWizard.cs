59e49e22f8a2e4f9a487608a4e1d8836c6837dc0
Josewowgame2888 <mistertec7777777@gmail.com>
2020-10-31 18:59:45 -0600

    SetupWizar work now

---
+++ PocketNET/Core/SetupWizard.cs
@@ -0,0 +1,92 @@
+﻿using PocketNET.Core.Config;
+using System;
+using System.IO;
+
+namespace PocketNET.Core
+{
+    public class SetupWizard
+    {
+        private const int DEFAULT_PORT = 19132;
+        private const int DEFAULT_PLAYERS = 100;
+        private const string DEFAULT_MOTD = "PocketNET Server";
+
+        private string route { get; set; }
+
+        private bool licence = false;
+        private int port = -1;
+        private int players = -1;
+        private string motd = "null";
+
+        public SetupWizard(string route)
+        {
+            this.route = route;
+        }
+
+        public bool Check()
+        {
+            if (!File.Exists(route + "server.yml"))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void Run()
+        {
+            do
+            {
+                if (GetInput("Accept the license of use?", "n", "y/N") == "y") licence = true;
+            }
+            while (licence == false);
+
+            do
+            {
+                port = Convert.ToInt32(GetInput("Insert the port on which the server will run", Convert.ToString(DEFAULT_PORT)));
+            }
+            while (port == -1);
+
+            do
+            {
+                players = Convert.ToInt32(GetInput("Insert the maximum number of players", Convert.ToString(DEFAULT_PLAYERS)));
+            }
+            while (players == -1);
+
+            do
+            {
+                motd = GetInput("Insert the server name", DEFAULT_MOTD);
+            }
+            while (motd == "null");
+
+            YamlConfig config = new YamlConfig(route + "server.yml");
+
+            config.Set("motd", motd);
+            config.Set("port", port);
+            config.Set("max-players", players);
+
+            config.Save();
+        }
+
+        private string GetInput(string message, string defaultInput = "", string options = "")
+        {
+            message = "[?] " + message;
+
+            if (options != "" || defaultInput != "")
+            {
+                message += " (" + (options == "" ? defaultInput : options) + ")";
+            }
+
+            message += ": ";
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            Console.WriteLine(message);
+
+            Console.ResetColor();
+
+            string input = Console.ReadLine();
+
+            return input == "" ? defaultInput : input;
+        }
+    }
+}
--- PocketNET/PocketNET.cs
+++ PocketNET/PocketNET.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using PocketNET.Core.Config;
+﻿using PocketNET.Core;
+using System;
+using System.IO;
 
 namespace PocketNET
 {
@@ -8,16 +8,20 @@ namespace PocketNET
     {
         static void Main(string[] args)
         {
-            JsonConfig config = new JsonConfig(@"C:\Users\Jose Luis\Downloads\Servidor/myjson.json");
+            Console.WriteLine("__________              __           __   _______  ______________________");
+            Console.WriteLine(@"\______   \____   ____ |  | __ _____/  |_ \      \ \_   _____/\__    ___/");
+            Console.WriteLine(@" |     ___/  _ \_/ ___\|  |/ // __ \   __\/   |   \ |    __)_   |    |   ");
+            Console.WriteLine(@" |    |  (  <_> )  \___|    <\  ___/|  | /    |    \|        \  |    |  ");
+            Console.WriteLine(@" |____|   \____/ \___  >__|_ \\___  >__| \____|__  /_______  /  |____|  ");
+            Console.WriteLine(@"                     \/     \/    \/             \/        \/            ");
 
-            config.Set("test", 1);
-            config.Set("test1", "jose");
-            config.Set("test2", true);
-            config.Save();
+            string dataPath = Directory.GetCurrentDirectory().Replace(@"PocketNET\PocketNET\bin\Debug\netcoreapp3.1", "") + "server/"; //// In case of compiling from the idea remove the extra folders
 
-            Console.WriteLine(config.GetInt32("test"));
-            Console.WriteLine(config.GetString("test1"));
-            Console.WriteLine(config.GetBool("test2"));
+            if (!Directory.Exists(dataPath)) Directory.CreateDirectory(dataPath);
+
+            SetupWizard wizard = new SetupWizard(dataPath);
+
+            if (!wizard.Check()) wizard.Run();
         }
     }
 }
